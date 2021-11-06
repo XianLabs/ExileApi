31f691181b84d1f67a26eac96e5a7e0682803776
XianLabs <44497570+XianLabs@users.noreply.github.com>
2021-11-06 19:11:18 -0400

    Added stuff..
    added camera zoom, hovered inventory mouse x and y, terrain ptr, reveal map ptr.

--- GameOffsets/IngameStateOffsets.cs
+++ GameOffsets/IngameStateOffsets.cs
@@ -16,12 +16,18 @@ namespace GameOffsets
         [FieldOffset(0x540)] public long UIHoverTooltip;
         [FieldOffset(0x57C)] public float MousePosX;
         [FieldOffset(0x580)] public float MousePosY;
+        [FieldOffset(0x600)] public int UIHoverCellX; // top left corner of currently hovered UI element, int version
+        [FieldOffset(0x604)] public int UIHoverCellY; // top left corner of currently hovered UI element, int version
         [FieldOffset(0x754)] public float TimeInGameF; // time since last frame in seconds
         [FieldOffset(0x758)] public float TimeInGame; // total time in game in seconds
+        [FieldOffset(0x768)] public long TerrainData; 
+        [FieldOffset(0x58)] public short IsMapRevealed; //2nd offset from TerrainData, gives you "is_map_revealed" flag.
         [FieldOffset(0x618)] public int DiagnosticInfoType;
         [FieldOffset(0x848)] public long LatencyRectangle;
         [FieldOffset(0xCE8)] public long FrameTimeRectangle;
         [FieldOffset(0xF38)] public long FPSRectangle;
         [FieldOffset(0x870)] public int Camera;
+        [FieldOffset(0xC08)] public float CameraZoom;   
+          
     }
 }
